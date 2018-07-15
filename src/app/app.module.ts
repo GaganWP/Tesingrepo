ce2fd1409741213c19ece6160e82e4907207068f
Patrick Sison <patrick.m.sison@gmail.com>
2018-07-15 03:22:15 -0400

    Added modules/packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AlertPageComponent } from './alert-page/alert-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TablePipe } from './services/table.pipe';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertPageComponent,
+    NavbarComponent,
+    TablePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ChartsModule,
+    PaginationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
